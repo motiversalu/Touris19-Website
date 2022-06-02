1dc346efa566c86ab9c5433642c4249efb7afcd6
SalifuSalu <brainstormbright@gmail.com>
2022-06-02 13:55:25 +0000

    first commit
    major folders and files

---
+++ touris19.py
@@ -0,0 +1,72 @@
+# python 3.10...
+from flask import Flask, render_template, url_for
+
+app = Flask(__name__)
+
+@app.route('/')
+@app.route('/home')
+def home():
+    return render_template('home.html')
+
+@app.route('/about')
+def about():
+    return render_template('about.html', title="About")
+
+@app.route('/login')
+def login():
+    return render_template('login.html', title="Login")
+
+@app.route('/register')
+def register():
+    return render_template('register.html', title="register")
+
+@app.route('/book')
+def book():
+    return render_template('book.html', title="book")
+
+@app.route('/sites')
+def sites():
+    return render_template('sites.html', title="sites")
+
+#individual sites
+@app.route('/kakum')
+def kakum():
+    return render_template("kakum.html", title="kakum national park")
+
+@app.route('/kwame')
+def kwame():
+    return render_template("kwame.html", title="Dr. Kwame Nkrumah Memorial Park".lower())
+
+@app.route('/bosomtwe')
+def bosomtwe():
+    return render_template("bosomtwe.html", title="lake bosomtwe")
+
+@app.route('/elmina')
+def elmina():
+    return render_template("elmina.html", title="elmina castle")
+
+@app.route('/accra')
+def accra():
+    return render_template("accra.html", title="accra")
+
+@app.route('/boti')
+def boti():
+    return render_template("boti.html", title="boti waterfalls")
+
+@app.route('/flag')
+def flag():
+    return render_template("flag.html", title="flag staff house")
+
+@app.route('/larabanga')
+def larabanga():
+    return render_template("larabanga.html", title="larabanga ancient mosque")
+
+@app.route('/afadja')
+def afadja():
+    return render_template("afadja.html", title="mount afadja")
+
+
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
